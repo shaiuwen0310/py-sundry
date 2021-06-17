ec9ebf96bc98b2bdc9df0dbcac6d8bd44a1a54af
shaiuwen0310 <shaiuwen0310@gmail.com>
2021-06-17 10:06:49 +0800

    init

--- gen_excel/test_excel.py
+++ gen_excel/test_excel.py
@@ -12,6 +12,9 @@ ws['A1'], ws['B1'], ws['C1'] = 'number1', 'number2', 'number3'
 try:
     # 可用loop去插值
     ws.append(["first", "second", "third"])
+    # 動態調整row和column
+    # ws.cell(row=row_num, column=(i + 1 + 3), value=qrcode_brightness_list[i])
+
 finally:
     # 在finally儲存檔案
     wb.save(datetime_str + '.xlsx')
